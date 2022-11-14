d3e6a55db1f7052d77729e4203c8bab43c7ba5b9
hhmadan <hhmadan03@gmail.com>
2022-11-14 18:36:01 +0530

    ADDED: UC1_Valid First Name

--- src/regexuserregistration/ValidateFirstName.java
+++ src/regexuserregistration/ValidateFirstName.java
@@ -1,8 +1,30 @@
 package regexuserregistration;
 
+import java.util.regex.Matcher;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class ValidateFirstName {
+    public static boolean validateFirstName(String fName){
+        String regex = "[A-z][a-z]{3,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(fName);
+        return matcher.matches();
+    }
+    public static void isValidFirstName(String fName){
+        if(validateFirstName(fName)){
+            System.out.println("Good Name "+fName);
+        }
+        else
+            System.out.println("Invalid Input..!\nPlease Enter Valid First Name with First Letter Capital And Minimum of 3 Letters");
+    }
 
     public static void main(String[] args){
-        System.out.println("WELCOME TO USER REGISTRATION");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String fName = sc.next();
+
+        validateFirstName(fName);       //validation for first name regex
+        isValidFirstName(fName);        //check if input matches the regex
     }
 }
