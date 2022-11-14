ead3c558f41c98deb917bc9c533e1df423332097
hhmadan <hhmadan03@gmail.com>
2022-11-14 18:33:11 +0530

    Welcome message on Master branch

---
+++ src/regexuserregistration/ValidateFirstName.java
@@ -0,0 +1,8 @@
+package regexuserregistration;
+
+public class ValidateFirstName {
+
+    public static void main(String[] args){
+        System.out.println("WELCOME TO USER REGISTRATION");
+    }
+}
